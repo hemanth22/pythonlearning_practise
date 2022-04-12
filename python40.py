7c2771b6227afd68a01e4e1de35c94e902d76d0a
hemanth22 <hemanth22hemu@gmail.com>
2022-04-12 11:06:34 +0000

    practise python40

---
+++ python40.py
@@ -0,0 +1,9 @@
+is_old = True
+is_licensed = True
+
+if is_old and is_licensed:
+  print("You are old enough to drive!")
+elif is_licensed:
+    print("You are licensed.")
+else:
+    print("You cannot drive")
