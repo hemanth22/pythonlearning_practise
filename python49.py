f1253c3b74ac6dc6fe8394bad3483669fd923ded
hemanth22 <hemanth22hemu@gmail.com>
2022-04-24 16:13:15 +0000

    feat: adding range

--- python49.py
+++ python49.py
@@ -1,2 +1,27 @@
 for number in range(1, 100):
     print(number)
+
+print('================')
+
+for _ in range(0, 10, 2):
+    print(_)
+
+print('================')
+
+for _ in range(0, 10, -1):
+    print(_)
+
+print('================')
+
+for _ in range(10, 0):
+    print(_)
+
+print('================')
+
+for _ in range(10, 0, -1):
+    print(_)
+
+print('================')
+
+for _ in range(2):
+    print(list(range(10)))
