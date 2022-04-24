cf62f937bb8c2f5c197a71f1c2f2f91017029a5d
hemanth22 <hemanth22hemu@gmail.com>
2022-04-24 08:41:41 +0000

    feat: adding false condition

--- python41.py
+++ python41.py
@@ -1,6 +1,6 @@
 # Ternary Operator
 
-# condition_if_true if condition else condition_if_else
+# condition_if_true if condition else condition_if_false
 
 is_friend = True
 can_message = "Message allowed" if is_friend else "Not allowed"
