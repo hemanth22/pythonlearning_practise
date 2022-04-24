f615fa4437d746ea6a51d2e35bd086f926c0b5de
hemanth22 <hemanth22hemu@gmail.com>
2022-04-24 14:30:45 +0000

    feat: adding iterable

---
+++ python47.py
@@ -0,0 +1,6 @@
+for item in (1,2, 3, 4, 5):
+    for x in ['a','b', 'c']:
+        print(item, x)
+
+#iterable - list, dictionary, tuple, set, string
+#iterate -> one by one check each item in the collection
