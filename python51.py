3a941736dd9ca1c7bfefe4e2ee1f5a732f3ff5ba
hemanth22 <hemanth22hemu@gmail.com>
2022-04-24 17:04:09 +0000

    feat: while loop

---
+++ python51.py
@@ -0,0 +1,29 @@
+i = 0
+while i < 50:
+    print(i)
+    break
+print('====================')
+i = 0
+while i < 50:
+    print(i)
+    i = i + 1
+print('====================')
+i = 0
+while i < 50:
+    print(i)
+    i += 1
+print('====================')
+i = 0
+while i < 50:
+    print(i)
+    i += 1
+else:
+    print('i is over 50')
+print('====================')
+i = 0
+while i < 50:
+    print(i)
+    i += 1
+    break
+else:
+    print('i is over 50')
