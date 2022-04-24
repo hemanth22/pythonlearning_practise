ef530f9fe23b5f83efa5d92735d95c05fe2d2b8d
hemanth22 <hemanth22hemu@gmail.com>
2022-04-24 14:24:52 +0000

    feat: loop1

---
+++ python46.py
@@ -0,0 +1,15 @@
+for item in 'Zero to Mastery':
+    print(item)
+
+for item in {1,2,3,4,5}:
+    print(item)
+
+for item in [1,2,3,4,5]:
+    print(item)
+
+for item in (1,2,3,4,5):
+    print(item)
+
+for item in (1,2, 3, 4, 5):
+    for x in ['a','b', 'c']:
+        print(item, x)
