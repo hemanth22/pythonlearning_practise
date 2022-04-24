459486cdf603fefcde294ce04c740c9e0ea84285
hemanth22 <hemanth22hemu@gmail.com>
2022-04-24 17:33:36 +0000

    feat: break, continue, pass

---
+++ python53.py
@@ -0,0 +1,37 @@
+my_list = [1,2,3]
+for item in my_list:
+    print(item)
+    break
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    break
+
+print('====================')
+
+my_list = [1,2,3]
+for item in my_list:
+    continue
+    print(item)
+
+i = 0
+while i < len(my_list):
+    i += 1
+    continue
+    print(my_list[i])
+
+print('====================')
+
+my_list = [1,2,3]
+for item in my_list:
+    pass
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    pass
+
+print('====================')
