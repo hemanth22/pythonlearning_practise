a2f907edb578af83813ae2a5c5ad3b1aa05aca94
hemanth22 <hemanth22hemu@gmail.com>
2022-04-24 14:40:54 +0000

    feat: iteration

--- python47.py
+++ python47.py
@@ -1,6 +1,24 @@
-for item in (1,2, 3, 4, 5):
-    for x in ['a','b', 'c']:
-        print(item, x)
+user = {
+    'name': 'Golem',
+    'age': 7,
+    'can_swim': False
+}
+
+for item in user.items():
+    print(item)
+
+for item in user.values():
+    print(item)
+
+for item in user.keys():
+    print(item)
+
+for x,y in user.items():
+    print(x,':',y)
+
+for item in user.items():
+    key,value = item;
+    print(key,'=',value)
 
 #iterable - list, dictionary, tuple, set, string
 #iterate -> one by one check each item in the collection
