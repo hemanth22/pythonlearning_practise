2f1f782d34b21309961c9c42d5da96a03e844d3f
hemanth22 <hemanth22hemu@gmail.com>
2022-05-08 06:33:38 +0000

    feat: update oops

---
+++ python67.py
@@ -0,0 +1,20 @@
+# OOP Object Oriented Program
+
+class BigObject: #Class
+    #Code
+    pass
+
+obj1 = BigObject() # instanciate the class
+obj2 = BigObject() # instanciate the class
+obj3 = BigObject() # instanciate the class
+
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type('hi'))
+print(type([]))
+print(type(()))
+print(type({}))
+print(type('a'))
+print(type(obj1))
