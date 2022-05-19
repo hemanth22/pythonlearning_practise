c27a6d0300439b6bdada189452e7c0f8afc29c21
hemanth22 <hemanth22hemu@gmail.com>
2022-05-19 16:27:37 +0000

    feat: inheritance example 2

---
+++ python80.py
@@ -0,0 +1,27 @@
+## Inheritance
+
+class User():
+    def sign_in(self):
+        print('Logged In')
+
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+    
+    def attack(self):
+        print(f'attacking with power of {self.power}')
+
+class Archer(User):
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
+    
+    def attack(self):
+        print(f'attacking with arrows: arrows left- {self.num_arrows}')
+
+wizard1 = Wizard('Merlin',50)
+archer1 = Archer('Merlin',50)
+wizard1.attack()
+archer1.attack()
+wizard1.sign_in()
